d87e49faf23185b73be6dabc8b95ba691b067a5a
Ivan Semkiv <ivansemkiv@MacBook-Air-Ivan.local>
2024-10-29 10:21:43 +0100

    LinkedList

--- LinkedList/ListNode.java
+++ LinkedList/ListNode.java
@@ -1,1 +1,42 @@
+package LinkedList;
+final class ListNode {
+    private final int val;
+    ListNode next;
 
+    public ListNode() {
+        this.val = 0;
+        this.next = null;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    private boolean isEmpty = true;
+
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        isEmpty = false;
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        if (isEmpty) {
+            return "[]";
+        }
+        StringBuilder list = new StringBuilder("[");
+        ListNode current = this;
+
+        while (current != null) {
+            list.append(current.val);
+            current = current.next;
+            if (current != null) {
+                list.append(",");
+            }
+        }
+        list.append("]");
+        return list.toString();
+    }
+}
---
+++ LinkedList/Solution.java
@@ -0,0 +1,33 @@
+package LinkedList;
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null && list2 == null) {
+            return new ListNode();
+        }
+
+        ListNode head = new ListNode();
+        ListNode cur = head;
+
+        while (list1 != null && list2 != null) {
+            if (list1.getVal() <= list2.getVal()) {
+                cur.next = list1;
+                list1 = list1.next;
+            }
+            else {
+                cur.next = list2;
+                list2 = list2.next;
+            }
+            cur = cur.next;
+        }
+
+        if (list1 == null) {
+            cur.next = list2;
+        }
+        else {
+            cur.next = list1;
+        }
+
+        return head.next;
+
+    }
+}
